afdb35fbd4731c15c5decbd6baf834d41f603aa0
Jayden Andrews <jaydenrandrews@gmail.com>
2026-04-14 15:25:11 -0400

    Implemented repositories and connected database

--- backend/src/main/java/com/scholarship/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/scholarship/backend/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.scholarship.backend.repositories;
 
-public interface UserRepository {
+import com.scholarship.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
