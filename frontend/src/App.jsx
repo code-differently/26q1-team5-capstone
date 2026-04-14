e680ce7b91c0b3bc6bf76f2502a862b38aca9951
James Kollilon Barclay III <jkbarclay261@gmail.com>
2026-04-14 17:05:35 -0400

    "updated App.jsx"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,16 +86,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <img src={heroImg} alt="Hero" className="hero" />
+      
       <h1>Scholarship Tracker</h1>
 
       <div className="card">
