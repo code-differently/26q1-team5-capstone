43a798ea0ca7c353602dbd7e5fe405ae2a15aaed
Jayden Andrews <jaydenrandrews@gmail.com>
2026-04-19 15:56:11 -0400

    Updated profile CSS

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -217,18 +217,33 @@ const ProfilePage = () => {
                   max="4.0"
                   value={editedProfile.gpa || ''}
                   onChange={(e) => {
-                    const val = e.target.value;
-                    handleProfileChange('gpa', val === '' ? null : parseFloat(val));
+                    const val = parseFloat(e.target.value);
+                    if (e.target.value === '') {
+                      handleProfileChange('gpa', null);
+                    } else if (!isNaN(val)) {
+                      const clamped = Math.min(4.0, Math.max(0, val));
+                      handleProfileChange('gpa', clamped);
+                    }
                   }}
                 />
               </div>
               <div className="form-group">
                 <label>Ethnicity:</label>
-                <input
-                  type="text"
+                <select
                   value={editedProfile.ethnicity || ''}
                   onChange={(e) => handleProfileChange('ethnicity', e.target.value)}
-                />
+                >
+                  <option value="">Select ethnicity</option>
+                  <option value="American Indian or Alaska Native">American Indian or Alaska Native</option>
+                  <option value="Asian">Asian</option>
+                  <option value="Black or African American">Black or African American</option>
+                  <option value="Hispanic or Latino">Hispanic or Latino</option>
+                  <option value="Middle Eastern or North African">Middle Eastern or North African</option>
+                  <option value="Native Hawaiian or Pacific Islander">Native Hawaiian or Pacific Islander</option>
+                  <option value="White">White</option>
+                  <option value="Two or More Races">Two or More Races</option>
+                  <option value="Prefer Not to Say">Prefer Not to Say</option>
+                </select>
               </div>
               <div className="form-group">
                 <label>Major:</label>
@@ -250,12 +265,73 @@ const ProfilePage = () => {
                 </select>
               </div>
               <div className="form-group">
-                <label>State:</label>
-                <input
-                  type="text"
+                <label>State/U.S. Territory:</label>
+                <select
                   value={editedProfile.state || ''}
                   onChange={(e) => handleProfileChange('state', e.target.value)}
-                />
+                >
+                  <option value="">Select state</option>
+                  <optgroup label="States">
+                    <option value="Alabama">Alabama</option>
+                    <option value="Alaska">Alaska</option>
+                    <option value="Arizona">Arizona</option>
+                    <option value="Arkansas">Arkansas</option>
+                    <option value="California">California</option>
+                    <option value="Colorado">Colorado</option>
+                    <option value="Connecticut">Connecticut</option>
+                    <option value="Delaware">Delaware</option>
+                    <option value="Florida">Florida</option>
+                    <option value="Georgia">Georgia</option>
+                    <option value="Hawaii">Hawaii</option>
+                    <option value="Idaho">Idaho</option>
+                    <option value="Illinois">Illinois</option>
+                    <option value="Indiana">Indiana</option>
+                    <option value="Iowa">Iowa</option>
+                    <option value="Kansas">Kansas</option>
+                    <option value="Kentucky">Kentucky</option>
+                    <option value="Louisiana">Louisiana</option>
+                    <option value="Maine">Maine</option>
+                    <option value="Maryland">Maryland</option>
+                    <option value="Massachusetts">Massachusetts</option>
+                    <option value="Michigan">Michigan</option>
+                    <option value="Minnesota">Minnesota</option>
+                    <option value="Mississippi">Mississippi</option>
+                    <option value="Missouri">Missouri</option>
+                    <option value="Montana">Montana</option>
+                    <option value="Nebraska">Nebraska</option>
+                    <option value="Nevada">Nevada</option>
+                    <option value="New Hampshire">New Hampshire</option>
+                    <option value="New Jersey">New Jersey</option>
+                    <option value="New Mexico">New Mexico</option>
+                    <option value="New York">New York</option>
+                    <option value="North Carolina">North Carolina</option>
+                    <option value="North Dakota">North Dakota</option>
+                    <option value="Ohio">Ohio</option>
+                    <option value="Oklahoma">Oklahoma</option>
+                    <option value="Oregon">Oregon</option>
+                    <option value="Pennsylvania">Pennsylvania</option>
+                    <option value="Rhode Island">Rhode Island</option>
+                    <option value="South Carolina">South Carolina</option>
+                    <option value="South Dakota">South Dakota</option>
+                    <option value="Tennessee">Tennessee</option>
+                    <option value="Texas">Texas</option>
+                    <option value="Utah">Utah</option>
+                    <option value="Vermont">Vermont</option>
+                    <option value="Virginia">Virginia</option>
+                    <option value="Washington">Washington</option>
+                    <option value="West Virginia">West Virginia</option>
+                    <option value="Wisconsin">Wisconsin</option>
+                    <option value="Wyoming">Wyoming</option>
+                  </optgroup>
+                  <optgroup label="Territories">
+                    <option value="American Samoa">American Samoa</option>
+                    <option value="District of Columbia">District of Columbia</option>
+                    <option value="Guam">Guam</option>
+                    <option value="Northern Mariana Islands">Northern Mariana Islands</option>
+                    <option value="Puerto Rico">Puerto Rico</option>
+                    <option value="U.S. Virgin Islands">U.S. Virgin Islands</option>
+                  </optgroup>
+                </select>
               </div>
               <div className="form-group">
                 <label>Career Goals:</label>
